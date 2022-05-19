e4417948e1324b28b167ab0e829d36260fd3a569
William Owalla <william.owalla@student.moringaschool.com>
2022-05-19 06:13:53 +0300

    add gsap animations

---
+++ app/static/js/script.js
@@ -0,0 +1,3 @@
+gsap.from('.intro,.desk' ,{duration: .7, opacity: 0, x: '-100%', stagger:.7})
+gsap.from('.mentor' ,{duration: 1, y: '-200%', ease: 'back',delay: 1.4})
+gsap.from('.student' ,{duration: 1, y: '-300%', ease: 'back' , delay: 2.4})
